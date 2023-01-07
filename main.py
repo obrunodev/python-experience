e0c31c63e2cd3bc09da1b81d1fff10670f2c8d58
obrunodev <brunorpdev@gmail.com>
2023-01-06 22:04:12 -0300

    path convertor

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
+
+db = [
+    {"username": "Cesar",},
+    {"username": "Bruno"},
+    {"username": "Rodrigo"},
+    {"username": "Tércio"},
+]
+
+
+class UserModel(str, Enum):
+    profile = "profile"
+    hello = "hello"
+    content = "content"
 
 
 @app.get("/")
@@ -8,6 +22,45 @@ async def hello_world():
     return {"msg": "Hello world"}
 
 
-@app.get("/{my_name}")
-async def say_hello(my_name: str) -> dict:
-    return {"my_name": my_name, "msg": f"Hello {my_name}!"}
+@app.get("/users/profile")
+async def get_user_profile():
+    return {
+        "id": 1,
+        "info": db[1],
+    }
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int) -> dict:
+    return {
+        "id": user_id,
+        "info": db[user_id],
+    }
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: UserModel):
+    if model_name == UserModel.profile:
+        return {
+            "model_name": model_name,
+            "msg": "Acesso ao perfil",
+        }
+    
+    if model_name == UserModel.hello:
+        return {
+            "model_name": model_name,
+            "msg": "Olá mundo",
+        }
+    
+    return {
+        "model_name": model_name,
+        "msg": "Conteúdo",
+    }
+
+
+# Usando caminhos como parâmetro
+@app.get("/files/{file_path:path}")
+async def get_file(file_path: str):
+    return {"file_path": file_path}
+
+# Parei em https://fastapi.tiangolo.com/tutorial/path-params/#path-convertor
